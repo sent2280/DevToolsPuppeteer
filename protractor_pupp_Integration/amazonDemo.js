21f9293aefee526bc0db45bb7c2bef639d7f9cd6
S Senthil Kumar <senkumar3@publicisgroupe.net>
2020-07-26 12:10:29 +0530

    added protractor puppeteer integeration scripts

--- protractor_pupp_Integration/amazonDemo.js
+++ protractor_pupp_Integration/amazonDemo.js
@@ -4,7 +4,18 @@ describe('Example suite', () => {
     
     it('Simple test', async () => {
 
-        await browser.get('https://angular.io/');
+        await browser.cdp.page.setRequestInterception(true);
+        
+        browser.cdp.page.on('response', async response => {
+            if (response.url() !== null) {
+            console.log('response url and status code\'s are ' + response.url() + response.status());
+            } else {
+                await response.continue();
+            }
+
+     })
+
+        await browser.get('https://www.amazon.in/');
 
     })
 })
